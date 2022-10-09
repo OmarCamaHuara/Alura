c9dc3a2d74a571a21fad5a3b38e84a9cf1ed821a
Domon <domon@pop-os.localdomain>
2022-10-09 09:51:58 -0300

    Atualizacao

--- 1_JAVA_OO/10_Quarkus/ManuelSpringDev/chapter-2-simple-project/src/main/java/org/acme/GreetingResource.java
+++ 1_JAVA_OO/10_Quarkus/ManuelSpringDev/chapter-2-simple-project/src/main/java/org/acme/GreetingResource.java
@@ -1,13 +1,9 @@
 package org.acme;
 
-import java.util.Optional;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
